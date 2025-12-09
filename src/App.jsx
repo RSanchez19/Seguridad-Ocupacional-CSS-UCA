2724d03dd7ab26ef70771d5a7ea8027827d40c53
RSanchez19 <00191018@uca.edu.sv>
2025-12-09 02:43:32 -0600

    fix: fixed padding in sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import ContactBar from './components/layout/ContactBar'
@@ -15,6 +16,13 @@ import {
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    if (window.location.hash) {
+      window.history.replaceState(null, '', window.location.pathname + window.location.search)
+      window.scrollTo({ top: 0, behavior: 'auto' })
+    }
+  }, [])
+
   return (
     <div className="App">
       <Header />
